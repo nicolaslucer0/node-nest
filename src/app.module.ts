78cf4989c566004510d9158fe84d716d2a409d62
Nicolas Lucero <nicolas.lucero@redb.ee>
2022-08-16 15:01:17 -0300

    feat: added resource store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StoreModule } from './store/store.module';
 
 @Module({
-  imports: [],
+  imports: [StoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/store/entities/store.entity.ts
@@ -0,0 +1,6 @@
+export class Store {
+  id: number;
+  name: string;
+  description: string;
+  location: string;
+}
